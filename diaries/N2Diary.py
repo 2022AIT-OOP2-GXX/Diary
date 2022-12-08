b6073744aba188e5f89b4d8a9c0a9c8d9ec6829a
Nagatani <nagatani@work-ark.jp>
2022-12-08 15:59:30 +0900

    add

---
+++ diaries/N2Diary.py
@@ -0,0 +1,11 @@
+from diaries.AbstractDiary import AbstractDiary
+
+class N2Diary(AbstractDiary):
+  def get_date(self):
+    return "2022-12-15"
+
+  def get_summary(self):
+    return "課題を何とか出し終えた。レポートの内容多すぎて、つらかった。"
+
+  def get_author(self):
+    return "ながたに"
