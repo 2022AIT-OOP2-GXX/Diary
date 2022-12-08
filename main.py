e29c1ca8d5c3b574072ec97eb0a08274b793b8a6
Nagatani <nagatani@work-ark.jp>
2022-12-08 15:52:32 +0900

    ながたにの日記を新規追加

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from diaries.DiarySample import DiarySample
+from diaries.NagataniDiary import NagataniDiary
 
 # ↓のリストには、メンバーの各⽇記が格納されます。
 diaries = [
-  DiarySample(), 
+  DiarySample(),
+  NagataniDiary(),
 ]
 
 for d in diaries:
